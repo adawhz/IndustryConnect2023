e1b92ecd4db78be08ba8c9081d385ba4bab5c065
Ada Zhang <ada.whz@gmail.com>
2023-03-04 15:13:45 +1300

    Handle exception with try and catch

--- IndustryConnect2023/Pages/LoginPage.cs
+++ IndustryConnect2023/Pages/LoginPage.cs
@@ -1,6 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-
+using NUnit.Framework;
 
 namespace IndustryConnect2023.Pages
 {
@@ -14,10 +14,16 @@ namespace IndustryConnect2023.Pages
 
             // Launch turnup portal
             driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
-           
-            //Identify username textbox and enter valid username
-            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
-            usernameTextbox.SendKeys("hari");
+            try
+            {
+                //Identify username textbox and enter valid username
+                IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+                usernameTextbox.SendKeys("hari");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("TurnUp portal home page did not launch.",ex.Message);
+            }
 
             //Identify password textbox and enter valid password
             IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
