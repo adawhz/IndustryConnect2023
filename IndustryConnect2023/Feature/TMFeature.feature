323e15adfcdb2056b42571e22d34081164f73b55
Ada Zhang <ada.whz@gmail.com>
2023-03-08 10:42:58 +1300

    Specflow implementation

---
+++ IndustryConnect2023/Feature/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a TurnUp portal admin
+I would like to create,edit and delete Time and Material records
+so that I can manage employees' time and materials successfully
+
+@tag1
+Scenario: Create new TM record with valid details
+	Given Logged in TurnUp portal successfully
+	When Navigate to TM page
+	And Create a new TM record
+	Then The new TM record should be created successfully
---
+++ IndustryConnect2023/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace IndustryConnect2023.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"Logged in TurnUp portal successfully")]
+        public void GivenLoggedInTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Navigate to TM page")]
+        public void WhenNavigateToTMPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Create a new TM record")]
+        public void WhenCreateANewTMRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The new TM record should be created successfully")]
+        public void ThenTheNewTMRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
